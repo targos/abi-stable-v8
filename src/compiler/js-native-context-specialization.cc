6470fa180f9d94e13c65899d11877805f109687e
Georg Neis <neis@chromium.org>
2021-09-20 16:14:48 +0200

    [LTS 9.5] Merged: [compiler] Fix a bug in PropertyCellData::Cache
    (cherry picked from commit 8589742831a3c09ac96e1e82bf4d697175f2a351)
    
    Bug: chromium:1250660, v8:7790
    Change-Id: Ib42580ac0c73a5f3c92008576410ff09a8b7b3de
    No-Try: true
    No-Presubmit: true
    No-Tree-Checks: true
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/3178967
    Reviewed-by: Maya Lekova <mslekova@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/9.5@{#24}
    Cr-Branched-From: 4a03d61accede9dd0e3e6dc0456ff5a0e3f792b4-refs/heads/9.5.172@{#1}
    Cr-Branched-From: 9a607043cb3161f8ceae1583807bece595388108-refs/heads/main@{#76741}

--- src/compiler/js-native-context-specialization.cc
+++ src/compiler/js-native-context-specialization.cc
@@ -970,6 +970,7 @@ Reduction JSNativeContextSpecialization::ReduceGlobalAccess(
         break;
       }
       case PropertyCellType::kUndefined:
+      case PropertyCellType::kInTransition:
         UNREACHABLE();
     }
   }
